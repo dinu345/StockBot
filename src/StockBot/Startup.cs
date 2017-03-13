467648e0694208e0c66fe31772043b142a305831
Dinu345 <dinesh1988@gmail.com>
2017-03-13 10:07:00 +0530

    minor fix

--- src/StockBot/Startup.cs
+++ src/StockBot/Startup.cs
@@ -21,17 +21,19 @@ namespace StockBot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseStaticFiles();
         }
     }
 }
